54ebac455e6411d3ce87b7aa73e6246f6dd9dae3
Ian N. Bennett <ian.bennett@microsoft.com>
2019-10-21 15:16:18 -0400

    Massive updates after working on a SmartApp using the SDK.  README and samples completely updated.

--- src/SmartThings.NETCoreWebHookSDK.Tests/SmartThings.NETCoreWebHookSDK.Tests/CryptoTests.cs
+++ src/SmartThings.NETCoreWebHookSDK.Tests/SmartThings.NETCoreWebHookSDK.Tests/CryptoTests.cs
@@ -1,3 +1,25 @@
+#region Copyright
+// <copyright file="CryptoTests.cs" company="Ian N. Bennett">
+//
+// Copyright (C) 2019 Ian N. Bennett
+// 
+// This file is part of SmartThings.NETCoreWebHookSDK.Tests
+//
+// SmartThings.NETCoreWebHookSDK.Tests is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SmartThings.NETCoreWebHookSDK.Tests is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/. 
+// </copyright>
+#endregion
+
 using ianisms.SmartThings.NETCoreWebHookSDK.Crypto;
 using System;
 using System.Collections.Generic;
