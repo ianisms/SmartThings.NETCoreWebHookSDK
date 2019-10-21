b3f7e231746166192c0e582e63f95c2e00f786a4
Ian N. Bennett <ian.bennett@microsoft.com>
2019-10-21 15:13:33 -0400

    Massive updates after working on a SmartApp using the SDK.  README and samples completely updated.

--- src/SmartThings.NETCoreWebHookSDK/Crypto/CryptoUtils.cs
+++ src/SmartThings.NETCoreWebHookSDK/Crypto/CryptoUtils.cs
@@ -1,9 +1,29 @@
-﻿using ianisms.SmartThings.NETCoreWebHookSDK.Extensions;
+﻿#region Copyright
+// <copyright file="CryptoUtils.cs" company="Ian N. Bennett">
+//
+// Copyright (C) 2019 Ian N. Bennett
+// 
+// This file is part of SmartThings.NETCoreWebHookSDK
+//
+// SmartThings.NETCoreWebHookSDK is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SmartThings.NETCoreWebHookSDK is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/. 
+// </copyright>
+#endregion
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
-using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.OpenSsl;
 using Org.BouncyCastle.Security;
@@ -12,7 +32,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
--- src/SmartThings.NETCoreWebHookSDK/WebhookHandlers/UninstallWebhookHandler.cs
+++ src/SmartThings.NETCoreWebHookSDK/WebhookHandlers/UninstallWebhookHandler.cs
@@ -1,4 +1,26 @@
-﻿using ianisms.SmartThings.NETCoreWebHookSDK.Utils.InstalledApp;
+﻿#region Copyright
+// <copyright file="UninstallWebhookHandler.cs" company="Ian N. Bennett">
+//
+// Copyright (C) 2019 Ian N. Bennett
+// 
+// This file is part of SmartThings.NETCoreWebHookSDK
+//
+// SmartThings.NETCoreWebHookSDK is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SmartThings.NETCoreWebHookSDK is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/. 
+// </copyright>
+#endregion
+
+using ianisms.SmartThings.NETCoreWebHookSDK.Utils.InstalledApp;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using System;
