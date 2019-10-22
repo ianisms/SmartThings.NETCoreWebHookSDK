eb71eb6f3bf4ea80bf17f193bf91031b0550840e
Ian N. Bennett <ian.bennett@microsoft.com>
2019-10-21 20:22:08 -0400

    License headers

--- src/SmartThings.NETCoreWebHookSDK.Tests/SmartThings.NETCoreWebHookSDK.Tests/CryptoTests.cs
+++ src/SmartThings.NETCoreWebHookSDK.Tests/SmartThings.NETCoreWebHookSDK.Tests/CryptoTests.cs
@@ -3,14 +3,14 @@
 //
 // Copyright (C) 2019 Ian N. Bennett
 // 
-// This file is part of SmartThings.NETCoreWebHookSDK.Tests
+// This file is part of SmartThings.NETCoreWebHookSDK
 //
-// SmartThings.NETCoreWebHookSDK.Tests is free software: you can redistribute it and/or modify
+// SmartThings.NETCoreWebHookSDK is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// SmartThings.NETCoreWebHookSDK.Tests is distributed in the hope that it will be useful,
+// SmartThings.NETCoreWebHookSDK is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
