781f6d8ade6cce3cabab45058def46d3bab0738c
ianisms <ianbe@microsoft.com>
2020-06-19 15:14:56 -0400

    Moving to MIT license

--- src/SmartThings.NETCoreWebHookSDK.Tests/SmartThings.NETCoreWebHookSDK.Tests/FileBackedTests.cs
+++ src/SmartThings.NETCoreWebHookSDK.Tests/SmartThings.NETCoreWebHookSDK.Tests/FileBackedTests.cs
@@ -1,7 +1,7 @@
 ﻿#region Copyright
-// <copyright file="SmartThingsAPIHelperTests.cs" company="Ian N. Bennett">
+// <copyright file="FileBackedTests.cs" company="Ian N. Bennett">
 //
-// Copyright (C) 2019 Ian N. Bennett
+// Copyright (C) 2020 Ian N. Bennett
 // 
 // This file is part of SmartThings.NETCoreWebHookSDK
 //
